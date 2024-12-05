296abe510bc6ea1a47ccf18fa983f74a7b9b4557
ASee <55654420+a-code-a@users.noreply.github.com>
2024-12-05 10:19:41 +0100

    init

--- agent.py
+++
@@ -1,7 +0,0 @@
-from crewai import crew, task, agent
-from crewai_tools import SerperDevTools
-from langchain_ibm import WatsonxLLM
-import os
-
-os.environment["antrophic_api_key"] = ""
-os.environ["SERPER_API_KEY"] = "e732b7720321deebef189e59177ff902c3fa4088"
